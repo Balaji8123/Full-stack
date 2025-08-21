9c35a1f46a1f319210d18cc7afa45212bc067505
Balajir <104020060+Balaji8123@users.noreply.github.com>
2025-08-21 19:13:12 +0530

    Update App.test.js

--- food-ordering-app/src/App.test.js
+++ food-ordering-app/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from './store'; // point to your Redux store
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
