b931805715f9e6a5f43c169cdb873d4db3314990
Balajir <104020060+Balaji8123@users.noreply.github.com>
2025-08-21 19:16:33 +0530

    Update App.test.js

--- food-ordering-app/src/App.test.js
+++ food-ordering-app/src/App.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from './store'; // point to your Redux store
+import stores from './stores'; // point to your Redux store
 import App from './App';
 
 test('renders learn react link', () => {
   render(
-    <Provider store={store}>
+    <Provider stores={stores}>
       <App />
     </Provider>
   );
